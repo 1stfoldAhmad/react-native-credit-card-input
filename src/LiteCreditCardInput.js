59678add78abbf00683fbf266e1d0fd597521579
Sajjad Ahmad <sevenstarahmad@gmail.com>
2024-07-01 14:34:29 +0500

    Update LiteCreditCardInput.js

--- src/LiteCreditCardInput.js
+++ src/LiteCreditCardInput.js
@@ -72,7 +72,7 @@ export default class LiteCreditCardInput extends Component {
 
     placeholders: PropTypes.object,
 
-    inputStyle: Text.propTypes.style,
+    inputStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 
     validColor: PropTypes.string,
     invalidColor: PropTypes.string,
